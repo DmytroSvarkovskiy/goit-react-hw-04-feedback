bc32c3f434938124d1249c8bba162f40468acc93
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-12-12 19:54:23 +0200

    -

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import { FeedbackBtn, FeedbackBtnWrapper } from './Feedbackoptions.styled';
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <FeedbackBtnWrapper>
+      {options.map(el => (
+        <FeedbackBtn key={el} onClick={onLeaveFeedback}>
+          {el}
+        </FeedbackBtn>
+      ))}
+    </FeedbackBtnWrapper>
+  );
+};
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
